64d071100233a37d5ba5deeb9bbcd0d9eb46d9f8
Erikc-R <erikcrodriguez13@gmail.com>
2025-06-29 11:49:54 -0400

    carga de curriculum

---
+++ script.js
@@ -0,0 +1,31 @@
+let descripcion= document.querySelector("descripcion-proyecto");
+let imagen = document.querySelector(".imagen-proyecto");
+
+let proyecto1 = "o";
+let descripcion1 = "o";
+let proyecto2 = "/imagenes/web_mario.png";
+let descripcion2 = "PROYECTO BASADO EN EL JUEGO DE MARIO BROS ";
+let proyecto3 = "/imagenes/web_arturos.png";
+let descripcion3 = "PROYECTO BASADO EN CREAR UNA PAGINA WEB DE UNA MARCA FAMOSA";
+let boton1 = document.getElementById("#boton1");
+let boton2 = document.getElementById("#boton2");
+let boton3 = document.getElementById("#boton3");
+
+boton1.addEventListener("click",function cambiarImagen1() {
+   
+
+
+
+})
+
+boton2.addEventListener("click",function cambiarImagen2() {
+    
+
+
+})
+
+boton3.addEventListener("click",function cambiarImagen3() {
+    
+
+
+})
